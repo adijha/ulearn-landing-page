81783da94b6a9df416806af3d2517be84883d4d1
Aditya Kumar Jha <mail@adijha.com>
2020-12-03 23:55:53 +0545

    linear progressbar also done

--- src/components/linearProgressbar/LinearProgressBar.js
+++ src/components/linearProgressbar/LinearProgressBar.js
@@ -1,7 +1,80 @@
 import React from "react";
-
+import './linearProgressBar.scss'
 export default function LinearProgressBar() {
+	const data = [
+		{
+			step: "L8",
+			currentStep: false,
+		},
+
+		{
+			step: "L7",
+			currentStep: false,
+		},
+
+		{
+			step: "L6",
+			currentStep: false,
+		},
+
+		{
+			step: "L5",
+			currentStep: false,
+		},
+
+		{
+			step: "L4",
+			currentStep: false,
+		},
+
+		{
+			step: "L3",
+			currentStep: false,
+		},
+
+		{
+			step: "L2",
+			currentStep: true,
+		},
+
+		{
+			step: "L1",
+			currentStep: false,
+		},
+	];
 	return (
-		<></>
+		<div className="linear-progressbar">
+			{data.map((item) => (
+				<>
+					<div>
+						<div
+            className="linear-progress"
+							style={{
+								
+								backgroundColor: item.currentStep ? "#009DFE" : "white",
+								color: item.currentStep ? "white" : "black",
+							}}
+						>
+							{item.step}
+						</div>
+
+						<p
+							style={{
+								color: item.currentStep ? "#009DFE" : "black",
+              }}
+              className="linear-progress-desc"
+
+						>
+							ممتاز
+						</p>
+					</div>
+					{item.step !== "L1" && (
+						<div style={{ flex: 1 }}>
+							<hr className="progress-hr" />
+						</div>
+					)}
+				</>
+			))}
+		</div>
 	);
 }
