0982b14896cfe181ba35234f6fd8edadd9a668ac
Aditya Kumar Jha <mail@adijha.com>
2020-12-04 13:13:59 +0545

    linear progress bar fix

--- src/components/linearProgressbar/LinearProgressBar.js
+++ src/components/linearProgressbar/LinearProgressBar.js
@@ -1,45 +1,53 @@
 import React from "react";
-import './linearProgressBar.scss'
+import "./linearProgressBar.scss";
 export default function LinearProgressBar() {
 	const data = [
 		{
 			step: "L8",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L7",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L6",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L5",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L4",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L3",
 			currentStep: false,
+			completed: false,
 		},
 
 		{
 			step: "L2",
 			currentStep: true,
+			completed: true,
 		},
 
 		{
 			step: "L1",
 			currentStep: false,
+			completed: true,
 		},
 	];
 	return (
@@ -48,29 +56,36 @@ export default function LinearProgressBar() {
 				<>
 					<div>
 						<div
-            className="linear-progress"
+							className="linear-progress"
 							style={{
-								
 								backgroundColor: item.currentStep ? "#009DFE" : "white",
-								color: item.currentStep ? "white" : "black",
+								color: item.currentStep
+									? "white"
+									: item.completed
+									? "#009DFE"
+									: "black",
 							}}
 						>
 							{item.step}
 						</div>
-
 						<p
 							style={{
 								color: item.currentStep ? "#009DFE" : "black",
-              }}
-              className="linear-progress-desc"
-
+							}}
+							className="linear-progress-desc"
 						>
 							ممتاز
 						</p>
 					</div>
 					{item.step !== "L1" && (
 						<div style={{ flex: 1 }}>
-							<hr className="progress-hr" />
+							<hr
+								style={{
+									border: `0.4px solid 
+								${item.completed ? "#009DFE" : "#eaebf7"}`,
+								}}
+								className="progress-hr"
+							/>
 						</div>
 					)}
 				</>
