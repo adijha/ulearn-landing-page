f9d2fa15f3e5f61c1d412eaddea3ba947703965a
Aditya Kumar Jha <mail@adijha.com>
2020-12-04 15:08:37 +0545

    time-shedule-component-ready

--- src/components/timeShedule/SheduleTime.js
+++ src/components/timeShedule/SheduleTime.js
@@ -5,8 +5,10 @@ export default function SheduleTime() {
 		<div className="shedule-container">
 			<div className="shedule-up-padding"></div>
 			<div className="shedule-header">
-				<p style={{ fontSize: "22px", fontWeight: "bold" }}>حصص جماعية</p>
-				<p>الحصص الجماعية مكونه من 5 الى 10 طلاب</p>
+				<p style={{ fontSize: "24px", fontWeight: "bold" }}>حصص جماعية</p>
+				<p
+				style={{fontSize:'19px'}}
+				>الحصص الجماعية مكونه من 5 الى 10 طلاب</p>
 				<hr style={{ border: "1px solid #ededed" }} />
 			</div>
 
@@ -23,7 +25,9 @@ export default function SheduleTime() {
 					className="shedule-radio"
 					>
 						<p style={{ fontSize: "18px" }}>{i}</p>
-						<input type="radio" id={i} value={i}></input>
+						<input
+						className="radio-btn"
+						type="radio" id={i} value={i}></input>
 					</div>
 				))}
 			</div>
